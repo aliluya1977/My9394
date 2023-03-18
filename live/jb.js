ff8552375fbc5e66aaec250175d677f5ff292b54
qizhen15800 <qizhen15800@163.com>
2023-03-18 03:22:17 +0000

    更新 'live/jb.js'

--- live/jb.js
+++ live/jb.js
@@ -1,4 +1,3 @@
-[
-{"name": "不良帅MV","url": "https://notabug.org/qizhen15800/My9394/raw/master/live/liveMTV.txt&&&https://notabug.org/qizhen15800/My9394/raw/master/live/%e4%b8%8d%e8%89%af%e5%b8%851.png"},
-{"name": "不良帅m3u", "url": "https://notabug.org/qizhen15800/My9394/raw/master/live/m3u.txt&&&https://notabug.org/qizhen15800/My9394/raw/master/live/%e4%b8%8d%e8%89%af%e5%b8%851.png"}
+{"name": "不良帅MV","url":"https://notabug.org/qizhen15800/My9394/raw/master/live/liveMTV.txt&&&https://notabug.org/qizhen15800/My9394/raw/master/live/%e4%b8%8d%e8%89%af%e5%b8%851.png"},
+{"name": "不良帅m3u","url":"https://notabug.org/qizhen15800/My9394/raw/master/live/m3u.txt&&&https://notabug.org/qizhen15800/My9394/raw/master/live/%e4%b8%8d%e8%89%af%e5%b8%851.png"}
 ]
