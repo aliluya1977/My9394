ce5e3d5b062387864bb2847312ec06ee8a85929e
qizhen15800 <qizhen15800@163.com>
2023-03-18 11:30:44 +0000

    更新 'live/jb.js'

--- live/jb.js
+++ live/jb.js
@@ -1,6 +1,6 @@
 [
-{"name": "不良帅MV","url":"https://notabug.org/qizhen15800/My9394/raw/master/live/liveMTV.txt&&&https://notabug.org/qizhen15800/My9394/raw/master/live/不良帅.JPG"},
-{"name": "不良帅m3u","url":"https://notabug.org/qizhen15800/My9394/raw/master/live/女团.txt&&&https://notabug.org/qizhen15800/My9394/raw/master/live/不良帅.JPG"},
-{"name": "不良帅m3u","url":"https://notabug.org/qizhen15800/My9394/raw/master/live/女团.txt&&&https://notabug.org/qizhen15800/My9394/raw/master/live/不良帅.JPG"},
-{"name": "不良帅DJ","url":"https://notabug.org/qizhen15800/My9394/raw/master/live/DJ.txt&&&https://notabug.org/qizhen15800/My9394/raw/master/live/不良帅.JPG"}
+{"name": "不良帅MV","url":"https://notabug.org/qizhen15800/My9394/raw/master/live/liveMTV.txt"},
+{"name": "不良帅女团","url":"https://notabug.org/qizhen15800/My9394/raw/master/live/女团.txt"},
+{"name": "不良帅m3u","url":"https://notabug.org/qizhen15800/My9394/raw/master/live/女团.txt"},
+{"name": "不良帅DJ","url":"https://notabug.org/qizhen15800/My9394/raw/master/live/DJ.txt"}
 ]
