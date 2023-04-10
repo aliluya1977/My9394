bb44354fb1656ceb743d8c36cbeba96dbc6e618f
qizhen15800 <qizhen15800@163.com>
2023-04-10 16:15:41 +0000

    添加 'alist.js'

---
+++ alist.js
@@ -0,0 +1,73 @@
+{
+  "drives": [
+  {
+     "name": "🎢arhome",
+     "server": "https://xn--ar-4g8e.tk"
+    },
+   {
+      "name": "👨‍🎓Lan同学",
+      "server": "https://l.lantongxue.repl.co"
+    },	
+   {
+      "name": "💢姹莱坞",
+      "server": "https://a.aa01.ml",
+	  "passwd": "agree"
+    },
+    {
+     "name": "🌟云星",
+     "server": "http://alist.itvbox.cc"
+    },	
+    {
+     "name": "🧬杜比",
+     "server": "https://dubi.tk"
+    },
+    {
+     "name": "💫触光",
+     "server": "https://pan.ichuguang.com"
+    },
+    {
+	 "name":"🌬️八面来风",
+     "server":"https://z7e8w0-5244.csb.app"
+    },
+    {
+      "name": "月夕晨",
+      "server": "http://alist.calendre.top/"
+    },
+    {
+      "name": "丫仙女",
+      "server": "http://alist.xiaoya.pro/"
+    },
+    {
+      "name": "日更",
+      "server": "http://e39325i849.zicp.fun:14725/"
+    },
+    {
+      "name": "目瞪狗呆",
+      "server": "https://pan.mdgd.cc/"
+    },
+    {
+      "name": "动漫",
+      "server": "http://pan.smjc.cc/"
+    },
+    {
+      "name": "星梦",
+      "server": "https://pan.bashroot.top"
+    },
+    {
+      "name": "一只鱼",
+      "server": "https://alist.youte.ml"
+    },
+    {
+      "name": "七米蓝",
+      "server": "https://al.chirmyram.com"
+    },
+    {
+      "name": "云播放",
+      "server": "https://quanzi.laoxianghuijia.cn"
+    },
+    {
+      "name": "讯维",
+      "server": "https://pan.xwbeta.com"
+    }
+  ]
+}
